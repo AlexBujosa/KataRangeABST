b5cb8e9fc438e3fee84a2694fe227eb41c83a452
Alex Jose Bujosa Cruz <bujosa2014@outlook.com>
2022-06-15 23:13:13 -0400

    Adding endPointTest and Feature of endPoints

--- KataRangeABST/Program.cs
+++ KataRangeABST/Program.cs
@@ -8,6 +8,67 @@ namespace KataRangeABST
 {
     class Program
     {
+        public enum SymbolOpen
+        {
+            CorcheteAbierto = 0,
+            ParentisAbierto = 1
+        }
+        public enum SymbolClose
+        {
+            CorcheteCerrado = 2,
+            ParentesisCerrado = 3
+        }
+
+        public static string GetAllPoints()
+        {
+            return "";
+        }
+        public static string endPoints(SymbolOpen Op, SymbolClose Cl, int[]rangeValue)
+        {
+            int fValue = rangeValue[0];
+            int sValue = rangeValue[1];
+            string OpenSymbol = "";
+            string CloseSymbol = "";
+            string OpenBraket = "{";
+            string CloseBraket = "}";
+            if (SymbolOpen.CorcheteAbierto ==  Op)
+            {
+                OpenSymbol = "[";
+            }
+            else
+            {
+                OpenSymbol = "(";
+                fValue += 1;
+            }
+
+            if (SymbolClose.CorcheteCerrado == Cl)
+            {
+               CloseSymbol = "]";
+            }
+            else
+            {
+                CloseSymbol = ")";
+                sValue -= 1;
+            }
+
+            return $"{OpenSymbol}{rangeValue[0]}, {rangeValue[1]}{CloseSymbol}  endPoints = {OpenBraket}{fValue}, {sValue}{CloseBraket}";
+        }
+        public static string integerRangeContain()
+        {
+            return "";
+        }
+        public static string equals()
+        {
+            return "";
+        }
+        public static string OverlapRange()
+        {
+            return "";
+        }
+        public static string ContainsRange()
+        {
+            return "";
+        }
         static void Main(string[] args)
         {
         }
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -6,8 +6,33 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void endPointsTest1()
         {
+            int[] range = { 2, 6 };
+            string result = "";
+            Assert.AreEqual("[2,6) endPoints = {2,5}", result);
         }
+        [TestMethod]
+        public void endPointsTest2()
+        {
+            int[] range = { 2, 6 };
+            string result = "";
+            Assert.AreEqual("[2,6] endPoints = {2,6}", result);
+        }
+        [TestMethod]
+        public void endPointsTest3()
+        {
+            int[] range = { 2, 6 };
+            string result = "";
+            Assert.AreEqual("(2,6) endPoints = {3,5}", result);
+        }
+        [TestMethod]
+        public void endPointsTest4()
+        {
+            int[] range = { 2, 6 };
+            string result = "";
+            Assert.AreEqual("(2,6] endPoints = {3,6}", result);
+        }
+
     }
 }
